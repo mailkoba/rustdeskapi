eb95a77fdd315dbfd5a41a5f7ae948fc1ae40f4b
Vadim Paramoshkin <mailkoba@gmail.com>
2023-08-27 17:24:41 +0400

    fix login response model for lastest client

--- RustDeskApi/Models/LoginResultModel.cs
+++ RustDeskApi/Models/LoginResultModel.cs
@@ -9,6 +9,8 @@ namespace RustDeskApi.Models
         [JsonPropertyName("access_token")]
         public string AccessToken { get; set; }
 
+        public string Type { get; set; } = "access_token";
+
         public UserModel User { get; set; }
     }
 }
